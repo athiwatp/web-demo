fa4068c06ced6e94c43ba6104aae76eba8fea40c
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-12-13 20:33:22 +0700

    Add view engine

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require('express')
 var app = express()
+var ejs = require('ejs')
 app.listen(2000)
+app.engine('html', ejs.renderFile)
 
+app.get('/', showIndex)
+app.get('/login', showLogin)
 
+function showIndex(req, res) {
+	res.render('index.html')
+}
+
+function showLogin(req, res) {
+	res.render('login.html')
+}
